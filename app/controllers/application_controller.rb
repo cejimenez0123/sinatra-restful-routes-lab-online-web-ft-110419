494f5d043c70d7d4431ca040a8387f96f4c8977c
Christian Jimenez <christianjimenez0123@gmail.com>
2020-01-14 20:06:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,10 @@ class ApplicationController < Sinatra::Base
    @recipes = Recipe.all
    erb :index
  end
+ post '/recipes' do  
+   @recipe = Recipe.create(params)
+   redirect to "/recipes/#{@recipe.id}"
+ end
  get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     erb :show
