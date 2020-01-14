919cd8fd6347f7cece5517271cb611e0ea68091c
Christian Jimenez <christianjimenez0123@gmail.com>
2020-01-14 19:48:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,20 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  get '/recipes/new' do
+    erb :new
+  end
 
-  # code actions here!
-
+  get '/recipes' do
+   @recipes = Recipe.all
+   erb :index
+ end
+ get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+  get '/recipes/:id/edit' do #loads edit form
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
 end
